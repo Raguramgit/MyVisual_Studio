e5bc266faa95fff8498580532f6ef60ecde4ee5d
Raguraman7 <raguram7393@gmail.com>
2022-06-20 14:47:46 +0530

    changes updated

--- Delete_keypair.py
+++ Delete_keypair.py
@@ -1,7 +1,3 @@
 import boto3
-client = boto3.resource('ec2')
-response = client.delete_key_pair(
-    KeyName='testing123',
-    KeyPairId='key-00080e19217d07715',
-    DryRun=True|False
-)
+x = boto3.resource('ec2')
+response = x.delete_key_pair(KeyName='testing123',KeyPairId='key-00080e19217d07715')
