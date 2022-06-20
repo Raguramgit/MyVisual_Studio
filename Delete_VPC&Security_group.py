247720ced97075726dda82dae2283b95f4d20c69
Raguraman7 <raguram7393@gmail.com>
2022-06-20 17:34:00 +0530

    Changes done

--- Delete_VPC&Security_group.py
+++ Delete_VPC&Security_group.py
@@ -1,9 +1,9 @@
 from http import client
 import boto3
 client = boto3.client('ec2')
-"""ec2 = client.delete_security_group(
+"ec2 = client.delete_security_group(
     GroupId='sg-0ccb22e1f593ef7f3',
-)"""
+)
 ec2 = client.delete_subnet(
     SubnetId='subnet-069064dfefa6ff75a',
 )
