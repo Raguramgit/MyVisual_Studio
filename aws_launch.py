af89588dfc9a286c92786eaa17c41b73649fd92a
Raguraman7 <raguram7393@gmail.com>
2022-06-20 12:19:32 +0530

    Changed

--- aws_launch.py
+++ aws_launch.py
@@ -1,9 +1,3 @@
-
 import boto3
-ec2 = boto3.resource('ec2')
-
-# create a new EC2 instance
-# MaxCount = defines the no of instances to launch
-# ImageID = dont forget to add image id
-
-ec2.create_instances(ImageId='ami-0c802847a7dd848c0',MinCount=1,MaxCount=3,InstanceType='t2.micro',KeyName='AWSkey')
+x = boto3.resource('ec2')
+x.create_instances(ImageId='ami-0c802847a7dd848c0',InstanceType='t2.micro',KeyName='AWSkey',MinCount=1,MaxCount=3,)
