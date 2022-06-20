6a3f274311db10f7d40ff9f0e32c4fbb3f87e7d1
Raguraman7 <raguram7393@gmail.com>
2022-06-20 14:48:57 +0530

    changes updated

--- Volumn_creation.py
+++ Volumn_creation.py
@@ -1,4 +1,3 @@
-from re import X
 import boto3
-x = boto3.resource('ec2')
-x.create_volume(AvailabilityZone='ap-southeast-1a',Size=10,VolumeType='gp3')
+ec2 = boto3.resource('ec2')
+ec2.create_volume(AvailabilityZone='ap-southeast-1a',Size=10,VolumeType='gp3')
