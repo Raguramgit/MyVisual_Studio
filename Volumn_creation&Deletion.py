ae403625140aea0daf5262cdb5b59a26e12a6772
Raguraman7 <raguram7393@gmail.com>
2022-09-27 10:49:53 +0530

    From Personal Laptop

--- Volumn_creation&Deletion.py
+++ Volumn_creation&Deletion.py
@@ -1,6 +1,6 @@
 import boto3
 ec2 = boto3.resource('ec2')
 ec2.create_volume(AvailabilityZone='ap-southeast-1b',Size=10,VolumeType='gp3')
-#client = boto3.client('ec2')
-#client.delete_volume(
- #   VolumeId='vol-098688dd285f73dc8')
+client = boto3.client('ec2')
+client.delete_volume(
+    VolumeId='vol-098688dd285f73dc8')
