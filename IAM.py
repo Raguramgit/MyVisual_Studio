85af15568fd19fd5602f046323031af4b8d55eaf
Raguraman7 <raguram7393@gmail.com>
2022-09-29 17:22:20 +0530

    Changes comitted_29092022

--- IAM.py
+++ IAM.py
@@ -1,8 +1,8 @@
 import boto3
 iam = boto3.resource('iam')
 user = iam.User('Ram')
-"""user = user.create(
+user = user.create(
     Path='/',
     PermissionsBoundary='arn:aws:iam::aws:policy/AdministratorAccess',
-)"""
+)
 response = user.delete()
